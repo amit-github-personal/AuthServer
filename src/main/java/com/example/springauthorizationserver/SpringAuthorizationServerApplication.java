380e7f35c7a16194e51614b09586b1bd1aadd7bf
amitmishra <amit.mishra@ticketmaster.com>
2022-02-25 14:03:34 +0530

    Database Configuration update

--- src/main/java/com/example/springauthorizationserver/SpringAuthorizationServerApplication.java
+++ src/main/java/com/example/springauthorizationserver/SpringAuthorizationServerApplication.java
@@ -2,10 +2,8 @@ package com.example.springauthorizationserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class SpringAuthorizationServerApplication {
 
     public static void main(String[] args) {
